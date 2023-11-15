ec4c0c112d2ffbf62d829c657bdd60bd0e34daf1
Lamirol <lucas.dau65@gmail.com>
2023-11-15 19:53:59 +0100

    Fin TP1
    Initialisation de la dataclass Rumba + initialiastion de la matrice

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
-#def testEtatBut(etat) :
+from collection import deque
+from dataclass import dataclass as dc
 
-#def filsEtat(etat):
+@dataclass
+class Rumba:
+    rumba : [][]
 
-#def ajouterTete(e,liste):
+    def __init__(self, ligne, colonne):
+        self.rumba = self.get_rumba(ligne, colonne)
 
+    def get_rumba(self, ligne, colonne):
+        num = 1
+        matrice = [[None for i_colonne in range(colonne)] for i_ligne in range(ligne)]
+        for i_ligne in range(len(matrice)):
+            for i_colonne in range(len(matrice[i_ligne])-1):
+                matrice[i_ligne][i_colonne] = num
+                num += 1
+        return matrice
 
-def profondeurDAbord(depart):
-  enAttente = depart
-  vus = []
-  trouve = False
-  while enAttente != [] && trouve == False :
-    prochain = pop.enAttente
-    vus = vus.append(prochain)
-    if testEtatBut(prochain) :
-      trouve == True
-      return(True, prochain)
-    else :
-      for e in filsEtat(prochain) :
-        if !(e in vus):
-        enAttente = ajouterTete(e, enAttente)
-  if trouve == False:
-    return(False, depart)
+    def isTopPile
+
+    def topPile()
+
+    def isMovementPossible()
+
+    #def testEtatBut(etat) :                            #testEtatBut(etat) ; {prédicat caractérisant les buts}
+
+    def filsEtat(etat):                                 #filsEtat(etat) ; {retourne la liste des fils d'un état}
+        fils = []
+        truc =
+        if :
+            fils.append(truc)
+        return fils
+
+    def profondeurDAbord(depart):
+      enAttente = depart
+      vus = []
+      trouve = False
+      while enAttente != [] && trouve == False :        #Tant que la liste n'est pas vide ou qu'on a pas trouvé l'état
+        prochain = enAttente.popleft()                  #On récupère l'état en tête de liste dans la variable prochain
+        vus.append(prochain)                            #On ajout prochain à la liste des éléments vus
+        if testEtatBut(prochain) :                      #Si jsp quoi
+          trouve == True                                #L'état est trouvé
+          return(True, prochain)                        #On return Vraie t l'état
+        else :                                          #Sinon
+          for e in filsEtat(prochain) :                 #On parcours les états fils de l'état prochain
+            if !(e in vus):                             #Si ils n'ont pas été vus
+            enAttente.insert(0, e)                      #On les insert à l'indice 0 de la liste enAttente
+      if trouve == False:                               #Si l'état n'a pas été trouvé
+        return(False, depart)                           #On return Faux et la liste de départ
